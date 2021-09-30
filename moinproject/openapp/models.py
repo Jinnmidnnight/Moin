06b26dc74589ffd7e311ffde1342967af0ff70e0
Jin <vkstk0131@yonsei.ac.kr>
2021-09-30 13:11:00 +0900

    글 작성 model

--- moinproject/openapp/models.py
+++ moinproject/openapp/models.py
@@ -8,61 +8,66 @@ class Open_1(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_1", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_2(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_2", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_3(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_3", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_4(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_4", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_5(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_5", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_6(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_6", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_7(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_7", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_8(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_8", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
 
 class Open_9(models.Model):
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to = "openapp_1", null=True, blank=True)
+    image = models.ImageField(upload_to = "Open_9", null=True, blank=True)
     number = models.IntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(50)], null=True, blank=True)
+
+class Write_1(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to = "Write_1", null=True, blank=True)
