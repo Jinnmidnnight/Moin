5dc2f7106ee235076d4b50dabc57f89573ab1bf5
Jin <vkstk0131@yonsei.ac.kr>
2021-10-13 21:55:40 +0900

    모임장 관리html 생성

--- moinproject/mypageapp/urls.py
+++ moinproject/mypageapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from mypageapp.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('mypage/', views.mypage, name="mypage"),
+    path('mypage/', mypage, name="mypage"),
+    path('update_1/<str:open_id>', update_1, name="update_1"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
