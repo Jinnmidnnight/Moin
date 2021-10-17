d884a636dd33819f487e678dee22e7e0ca89713f
Jin <vkstk0131@yonsei.ac.kr>
2021-10-18 01:22:46 +0900

    모임장 관리 기능

--- moinproject/mypageapp/views.py
+++ moinproject/mypageapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import auth
 from account.models import User
+from writeapp.models import Write_1
 from openapp.models import Open_1
 # Create your views here.
 def joinin_wait(request, open_id):
@@ -10,6 +11,20 @@ def joinin_wait(request, open_id):
 def joinin(request, open_id):
     open = get_object_or_404(Open_1, pk = open_id)
     return render(request, 'joinin.html', {'open' : open})
+
+def delete_1(request, open_id):
+    open = get_object_or_404(Open_1, pk = open_id)
+    open.delete()
+    return redirect('mypage')
+
+def write_delete_1(request, write_id):
+
+    write = get_object_or_404(Write_1, pk=write_id)
+    post = get_object_or_404(Open_1, pk = write.post.id)
+    
+    write.delete()
+   
+    return redirect('manage_1', post.id)
 
 def deport_1(request, open_id):
     open = get_object_or_404(Open_1, pk = open_id)
@@ -31,7 +46,7 @@ def update_1(request, open_id):
     open.image = request.FILES.get('image')
     open.number = request.POST['number']
     open.save()
-    return redirect('detail_1', open.id)
+    return redirect('manage_1', open.id)
 
 def mypage(request):
     open_index = Open_1.objects.all()
