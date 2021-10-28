dfc628c71f83a4f0321841458e411b13c075beda
댁준 <ksjoon28@naver.com>
2021-10-28 19:22:10 +0900

    2021/10/28

--- moinproject/scheduleapp/views.py
+++ moinproject/scheduleapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from openapp.models import Open_1
+from scheduleapp.models import Schedule_1
 
 # Create your views here.
 
@@ -12,5 +13,15 @@ def schedule_1(request, open_id):
     return render(request, 'schedule_1.html', {'open' : open})
 
 def schedule_making(request, open_id):
-    open = get_object_or_404(Open_1, pk = open_id)
-    return render(request, 'schedule_making.html', {'open' : open})
+    # open = get_object_or_404(Open_1, pk = open_id)
+    # return render(request, 'schedule_making.html', {'open' : open})
+    if request.method == 'GET':
+        return render(request, "open_1.html")
+    
+    schedule = Schedule_1.objects.create()
+    schedule.title = request.POST['title']
+    schedule.date = request.POST['date']
+    schedule.location = request.POST['location']
+    schedule.price = request.POST['price']
+    schedule.save()
+    return redirect('schedule_1', open.id)
