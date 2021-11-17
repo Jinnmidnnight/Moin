257a2204eeac9283f962629d75718a1febd67f9a
댁준 <ksjoon28@naver.com>
2021-11-17 15:57:26 +0900

    2021/11/17

--- moinproject/scheduleapp/urls.py
+++ moinproject/scheduleapp/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('schedule_1/<str:open_id>', schedule_1, name="schedule_1"),
     path('schedule_make/<str:open_id>', schedule_make, name="schedule_make"),
-    path('schedule_making/<str:open_id>', schedule_making, name="schedule_making"),
+    path('schedule_making_1/<str:open_id>', schedule_making_1, name="schedule_making_1"),
+    path('schedule_detail_1/<str:schedule_id>', schedule_detail_1, name="schedule_detail_1"),
 ]
